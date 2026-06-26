8c65c1bbfee88d7a50e744845a5d055036f106bb
Eclipse <20222062@eaut.edu.vn>
2026-06-27 01:36:19 +0700

    Fix: properly disable SSL cert verify (array_filter was removing false)

--- config/database.php
+++ config/database.php
@@ -61,8 +61,7 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-            ]) : [],
+            ]) + [PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false] : [],
         ],
 
         'mariadb' => [
@@ -82,8 +81,7 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-            ]) : [],
+            ]) + [PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false] : [],
         ],
 
         'pgsql' => [
